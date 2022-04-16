77ff117d0a9d71ec9dca0bc62ccadc5adb3d81b0
Mesut Öztürk <mesutozturk@users.noreply.github.com>
2022-04-16 14:58:50 +0300

    .

--- Gün-3/js/cift.js
+++ Gün-3/js/cift.js
@@ -1,14 +1,18 @@
-var a = [1,2,3,4,5,6,6,8,9,10]; //return 6
+var a = [1,2,3,4,5,6,6,8,9,10,2,2]; //return 6
 var b = [2,4,5,3,1,1,3,4,5,5]; //retrun 1,5
 
 function checkDistinct(array) {
-    for (var i = 1; i < array.length; i++) {
-        var item1 = array[i-1];
-        var item2 = array[i];
-        if(item1 == item2) {
-            return item1;
+    var sortedArray = array.sort();
+    //console.log(sortedArray);
+    var distinct = [];
+    for (var i = 1; i < sortedArray.length; i++) {
+        var item1 = sortedArray[i-1];
+        var item2 = sortedArray[i];
+        if (item1 == item2 && distinct.indexOf(item1) == -1) {
+            distinct.push(item1);
         }
     }
+    return distinct;
 }
 
 console.log(checkDistinct(a));
