9f81cddc3e84bd40d112f31326ee9057f47ccfb3
Mesut Öztürk <mesutozturk@users.noreply.github.com>
2022-04-16 14:02:28 +0300

    fibonacci ve faktoriel hesaplama

--- Gün-3/js/kosullar.js
+++ Gün-3/js/kosullar.js
@@ -1,5 +1,12 @@
 var hesapla = function () {
   var sayi = parseInt(document.getElementById("txtSayi").value);
+  if (!isNaN(sayi)) {
+    var fibo = fibonacci(sayi);
+    console.log(fibo);
+
+    var ff = faktoriel(sayi);
+    console.log(ff);
+  }
   if (sayi >= 1 && sayi <= 6) {
     zarAt(sayi);
   } else if (isNaN(sayi)) {
@@ -14,8 +21,8 @@ var zarAt = function (sayi) {
   var sayac = 0;
   while (true) {
     var zar1 = Math.floor(Math.random() * 6) + 1;
-      var zar2 = Math.floor(Math.random() * 6) + 1;
-      console.log(zar1 + " " + zar2);
+    var zar2 = Math.floor(Math.random() * 6) + 1;
+    //console.log(zar1 + " " + zar2);
     sayac++;
     if (zar1 == zar2 && sayi == zar1) {
       alert(sayac + ". denemede " + zar1 + " " + zar2 + " çifti geldi");
@@ -24,3 +31,21 @@ var zarAt = function (sayi) {
   }
   return sayac;
 };
+
+function fibonacci(sayi) {
+  if (sayi == 0) {
+    return 0;
+  } else if (sayi == 1) {
+    return 1;
+  } else {
+    return fibonacci(sayi - 1) + fibonacci(sayi - 2);
+  }
+}
+
+function faktoriel(sayi) {
+  if (sayi == 0) {
+    return 1;
+  } else {
+    return sayi * faktoriel(sayi - 1);
+  }
+}
