f116cf4b01ac8a1501d843b523601296e7056160
Mesut Öztürk <mesutozturk@users.noreply.github.com>
2022-04-16 11:42:35 +0300

    listele table

--- Gün-3/js/sinifListesi.js
+++ Gün-3/js/sinifListesi.js
@@ -14,8 +14,29 @@ function ekle() {
     document.getElementById("txtAd").value = "";
     document.getElementById("txtSoyad").value = "";
     document.getElementById("txtAd").focus();
-    console.log(liste);
-    listele();
+    //console.log(liste);
+    // listele();
+    listeleTablo();
+}
+
+function listeleTablo() {
+    var table = document.getElementById("sinif-listesi");
+    table.innerHTML = "";
+
+    for (var i = 0; i < liste.length; i++) {
+        var item = liste[i];
+        var tr = document.createElement("tr");
+        var td1 = document.createElement("td");
+        var td2 = document.createElement("td");
+        var td3 = document.createElement("td");
+        td1.innerHTML = i + 1;
+        td2.innerHTML = item.ad;
+        td3.innerHTML = item.soyad;
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        table.appendChild(tr);
+    }
 }
 
 function listele() {
