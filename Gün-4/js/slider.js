5512dc57900d3ffda9d192115b64b284bede74ac
Mesut Öztürk <mesutozturk@users.noreply.github.com>
2022-04-17 10:44:13 +0300

    color invert

--- Gün-4/js/slider.js
+++ Gün-4/js/slider.js
@@ -6,8 +6,16 @@ function changeColor() {
     var blue = document.getElementById("in-blue").value;
 
     var color = "rgb(" + red + "," + green + "," + blue + ")";
-    var color2 = `rgb(${red},${green},${blue})`;
+    var colorInvert = `rgb(${255-red},${255-green},${255-blue})`; // string interpolation -- string format
+    var div = document.getElementById("dv-color");
 
+    div.innerHTML = color;
+    div.style.backgroundColor = color;
+    div.style.color = colorInvert;
+    
     console.log(color);
-    console.log(color2);
+    console.log(colorInvert);
 }
+
+//init function
+//changeColor();
