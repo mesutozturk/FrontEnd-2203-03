7bad35a34c5fd6fc764b38772c8a9d9caf0f6417
Mesut Öztürk <mesutozturk@users.noreply.github.com>
2022-04-17 10:24:04 +0300

    .

--- Gün-4/js/slider.js
+++ Gün-4/js/slider.js
@@ -1,1 +1,13 @@
 console.log("slider.js");
+
+function changeColor() {
+    var red = document.getElementById("in-red").value;
+    var green = document.getElementById("in-green").value;
+    var blue = document.getElementById("in-blue").value;
+
+    var color = "rgb(" + red + "," + green + "," + blue + ")";
+    var color2 = `rgb(${red},${green},${blue})`;
+
+    console.log(color);
+    console.log(color2);
+}
