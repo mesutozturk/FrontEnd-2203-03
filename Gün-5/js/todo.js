45f356079b7fe00cd98036de2087e19bec9e1148
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2022-04-24 14:42:03 +0300

    todo ok + localstorage

--- Gün-5/js/todo.js
+++ Gün-5/js/todo.js
@@ -8,11 +8,14 @@ function add() {
   }
   var todo = {
     text: txt.value,
-    isChecked: true,
+    isChecked: false,
     createdDate: new Date(),
   };
   todoList.push(todo);
+  saveLocalStroge();
   listItemGenerator();
+  txt.value = "";
+  txt.focus();
 }
 //Ternary If Operator ? :
 function listItemGenerator() {
@@ -23,16 +26,56 @@ function listItemGenerator() {
     var template = `<li class="list-group-item">
         <div class="row">
             <div class="col-1">
-                <input type="checkbox" class="form-check-input me-1">
+                <input type="checkbox" ${
+                  item.isChecked ? "checked" : ""
+                } class="form-check-input me-1 cb-state" data="${i}">
             </div>
-            <div class="col ${ item.isChecked ? "text-decoration-line-through" : "" }">
+            <div class="col ${
+              item.isChecked ? "text-decoration-line-through" : ""
+            }">
                 ${item.text} 
             </div>
             <div class="col-2">
-                <input type="button" value="Delete" class="btn btn-outline-danger btn-sm float-right">
+                <input type="button" value="Delete" class="btn btn-outline-danger btn-sm float-right btn-delete" data=${i}>
             </div>
         </div>
     </li>`;
     ul.innerHTML += template;
   }
+  var cbList = document.getElementsByClassName("cb-state");
+  for (var i = 0; i < cbList.length; i++) {
+    var item = cbList[i];
+    item.addEventListener("change", function () {
+      //callback function
+      var index = this.getAttribute("data");
+      todoList[index].isChecked = !todoList[index].isChecked;
+      var itemCol = this.parentNode.parentNode.children[1]; // 2 element üste çıktık 2. alt elementini seçtik bu seçtiğimiz itemCol bizim textimizi barındıran div olduğu için verdiğimiz class ile üstü çizik veya değil olacak
+      itemCol.classList.toggle("text-decoration-line-through");
+      saveLocalStroge();
+    });
+  }
+  var deleteBtnList = document.getElementsByClassName("btn-delete");
+  for (var i = 0; i < deleteBtnList.length; i++) {
+    var item = deleteBtnList[i];
+    item.addEventListener("click", function () {
+      var index = this.getAttribute("data");
+      todoList.splice(index, 1); //dizinin içindeki index numarasına göre 1 eleman siler
+      var parentLi = this.parentNode.parentNode.parentNode; //li elementini seçtik
+      parentLi.parentNode.removeChild(parentLi); // li'nin ul'sinden li elementini sildik
+      saveLocalStroge();
+    });
+  }
+}
+
+function checkLocalStroge() {
+  var myTodoList = localStorage.getItem("myTodoList");
+
+  if (myTodoList != null) {
+    todoList = JSON.parse(myTodoList);
+  }
+  listItemGenerator();
+}
+checkLocalStroge();
+function saveLocalStroge() {
+  localStorage.setItem("myTodoList", JSON.stringify(todoList));
 }
