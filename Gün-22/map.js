0de41355a2f324b3ef5ae431fffe6c9cc364fae0
Mesut Öztürk <mesutozturk@users.noreply.github.com>
2022-07-23 13:37:06 +0300

    google maps

--- Gün-22/map.js
+++ Gün-22/map.js
@@ -9,10 +9,71 @@ let konumBul = function () {
     }
 }
 
+let konum;
+
+function initMap() {
+    console.log("Google Map js kütüphanesi yüklendi");
+    if (konum == undefined) {
+        console.log("Konum bilgisi olmadığı için tekrar deneniyor");
+        setTimeout(initMap, 2000);
+    }
+    console.log(konum);
+    const mapDiv = document.getElementById("map");
+    const map = new google.maps.Map(mapDiv, {
+        center: konum,
+        zoom: 17
+    });
+
+    const trafficLayer = new google.maps.TrafficLayer();
+    trafficLayer.setMap(map);
+
+    // const marker = new google.maps.Marker({
+    //     position: konum,
+    //     map: map,
+    // });
+
+    map.addListener("click", (pos) => {
+        console.log("haritaya tıklandı");
+        console.log(pos);
+        let div = document.getElementById("konum");
+        div.innerHTML = `Enlem: ${pos.latLng.lat()} - Boylam: ${pos.latLng.lng()}`;
+        var newPos = {
+            lat: pos.latLng.lat(),
+            lng: pos.latLng.lng()
+        };
+        newMarker(map, newPos);
+    });
+
+}
+
+let newMarker = (map, position) => {
+    console.log("new marker");
+    console.log(position);
+    map
+    const marker = new google.maps.Marker({
+        position: position,
+        map: map,
+        draggable: true
+    });
+
+    marker.addListener("dragend", (pos) => {
+        console.log("dragend");
+        console.log(pos);
+    });
+
+    marker.addListener("click", () => {
+        marker.setMap(null);
+    });
+}
+
 let konumBulundu = function (positions) {
     console.log(positions);
     let div = document.getElementById("konum");
     div.innerHTML = `Enlem: ${positions.coords.latitude} - Boylam: ${positions.coords.longitude}`;
+    konum = {
+        lat: positions.coords.latitude,
+        lng: positions.coords.longitude
+    };
 }
 
 let konumBasarisiz = function (error) {
